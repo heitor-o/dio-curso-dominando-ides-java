791d477baeecefabceacb7cf395c74bdc9f817d9
heitor-o <hoheitor@gmail.com>
2022-05-15 14:41:33 -0300

    first commint

---
+++ src/com/dio/DioPrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class DioPrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+	System.out.println("Hello World! " + (a+b));*/
+
+	}
+
+}
